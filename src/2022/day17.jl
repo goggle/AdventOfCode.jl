11220a681405df0d54348d7fc13e074f8f7887e9
Alexander Seiler <seileralex@gmail.com>
2025-06-09 02:50:04 +0200

    [2022, Day 17] Optimize

--- src/2022/day17.jl
+++ src/2022/day17.jl
@@ -1,71 +1,100 @@
 function day17(input::String = readInput(joinpath(@__DIR__, "..", "..", "data", "day17.txt")))
     instructions = rstrip(input)
-    shapes = (fill(true, 1, 4), Bool[0 1 0; 1 1 1; 0 1 0], Bool[0 0 1; 0 0 1; 1 1 1], fill(true, 4, 1), fill(true, 2, 2))
+    shapes = (
+        [(0, 0), (0, 1), (0, 2), (0, 3)],           # horizontal line
+        [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)],   # plus
+        [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],   # L-shape
+        [(0, 0), (1, 0), (2, 0), (3, 0)],           # vertical line
+        [(0, 0), (0, 1), (1, 0), (1, 1)]            # square
+    )
+    
     diffs = part1(instructions, shapes, n = 2500)
     p1 = sum(diffs[1:2022])
+    
+    # Find cycle pattern
     cs = findall([all(diffs[101:101+200] .== diffs[i+100:i+100+200]) for i ∈ 1:2200])
     cycle = cs[2] - cs[1]
-    heightincrease = diffs[300:300+cycle] |> sum
-
+    heightincrease = sum(diffs[300:300+cycle])
     cycleadd = ((1_000_000_000_000 ÷ cycle) - 1) * heightincrease
     rest = mod(1_000_000_000_000, cycle) + cycle
     p2 = sum(diffs[1:rest]) + cycleadd
-
+    
     return [p1, p2]
 end
 
-function part1(instructions::AbstractString, shapes::NTuple{5, Matrix{Bool}}; n = 2022)
-    board = zeros(Bool, n * 4, 7)
-    board[end, :] .= true
-
+function part1(instructions::AbstractString, shapes::NTuple{5, Vector{Tuple{Int,Int}}}; n = 2022)
+    occupied = Set{Tuple{Int,Int}}()
+    
+    # Add floor
+    for j in 1:7
+        push!(occupied, (0, j))
+    end
+    
     instri = 1
     shapei = 1
-
     diffs = zeros(Int, n)
-    oldheighti = size(board, 1)
+    max_height = 0
+    
     for k ∈ 1:n
         shape = shapes[shapei]
-        highest_i = highest_rock(board)
-        i = (highest_i - 1) - 2 - size(shape, 1) - 1
-        j = 2
+        
+        # Start position: 3 units above highest rock, 2 units from left
+        start_row = max_height + 4
+        start_col = 3
+        
+        current_row, current_col = start_row, start_col
+        
         while true
+            # Apply jet push
             c = instructions[instri]
             instri = mod1(instri + 1, length(instructions))
+            
             if c == '>'
-                i, j, result = move_right(board, shape, i, j)
+                # Try to move right
+                if can_move(occupied, shape, current_row, current_col + 1)
+                    current_col += 1
+                end
             else
-                i, j, result = move_left(board, shape, i, j)
+                # Try to move left  
+                if can_move(occupied, shape, current_row, current_col - 1)
+                    current_col -= 1
+                end
             end
-            i, j, result = move_down(board, shape, i, j)
-            if !result
-                board[map(x -> x + CartesianIndex(i, j), findall(shape))] .= true
-                newheighti = highest_rock(board)
-                diffs[k] = oldheighti - newheighti
-                oldheighti = newheighti
+            
+            # Try to move down
+            if can_move(occupied, shape, current_row - 1, current_col)
+                current_row -= 1
+            else
+                # Can't move down - place the shape
+                new_max = place_shape!(occupied, shape, current_row, current_col, max_height)
+                diffs[k] = new_max - max_height
+                max_height = new_max
                 break
             end
         end
+        
         shapei = mod1(shapei + 1, length(shapes))
     end
+    
     return diffs
 end
 
-highest_rock(b::Matrix{Bool}) = minimum(minimum(findall(b[:, i])) for i ∈ axes(b, 2))
-
-function move_left(board::Matrix{Bool}, shape::Matrix{Bool}, i::Int, j::Int)
-    occupied = map(x -> x + CartesianIndex(i, j - 1), findall(shape))
-    (minimum(map(x -> x[2], occupied)) < 1 || any(board[occupied])) && return i, j, false
-    return i, j - 1, true
+@inline function can_move(occupied::Set{Tuple{Int,Int}}, shape::Vector{Tuple{Int,Int}}, row::Int, col::Int)
+    for (dr, dc) in shape
+        new_row, new_col = row + dr, col + dc
+        if new_col < 1 || new_col > 7 || (new_row, new_col) ∈ occupied
+            return false
+        end
+    end
+    return true
 end
 
-function move_right(board::Matrix{Bool}, shape::Matrix{Bool}, i::Int, j::Int)
-    occupied = map(x -> x + CartesianIndex(i, j + 1), findall(shape))
-    (maximum(map(x -> x[2], occupied)) > 7 || any(board[occupied])) && return i, j, false
-    return i, j + 1, true
-end
-
-function move_down(board::Matrix{Bool}, shape::Matrix{Bool}, i::Int, j::Int)
-    occupied = map(x -> x + CartesianIndex(i + 1, j), findall(shape))
-    any(board[occupied]) && return i, j, false
-    return i + 1, j, true
+@inline function place_shape!(occupied::Set{Tuple{Int,Int}}, shape::Vector{Tuple{Int,Int}}, row::Int, col::Int, current_max::Int)
+    new_max = current_max
+    for (dr, dc) in shape
+        pos = (row + dr, col + dc)
+        push!(occupied, pos)
+        new_max = max(new_max, row + dr)
+    end
+    return new_max
 end
